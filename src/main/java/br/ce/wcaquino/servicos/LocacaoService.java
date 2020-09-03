353fde807e1a72fddeaa2fefb130b3db88b3fc58
Wellington Max Cruz <wellington.max1@gmail.com>
2020-09-02 21:31:49 -0300

    Tratamento de novas exceções,

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -3,6 +3,8 @@ package br.ce.wcaquino.servicos;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
 
 import java.util.Date;
 
@@ -10,10 +12,16 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 public class LocacaoService {
 	
-	public Locacao alugarFilme(Usuario usuario, Filme filme) throws Exception {
+	public Locacao alugarFilme(Usuario usuario, Filme filme) throws FilmeSemEstoqueException, LocadoraException {
+		if (usuario == null)
+			throw new LocadoraException("Usuário vazio");
+
+		if (filme == null)
+			throw new LocadoraException("Filme vazio");
+
 		// verificação se o filme tem estoque - regra de negocio
 		if (filme.getEstoque() == 0)
-			throw new Exception("Filme sem estoque.");
+			throw new FilmeSemEstoqueException();
 
 		// criacao da locação
 		Locacao locacao = new Locacao();
