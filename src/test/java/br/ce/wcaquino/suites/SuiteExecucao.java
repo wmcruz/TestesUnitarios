81708cdb411def2ad83f4c7de4f3c2b78600cf21
Wellington Max Cruz <wellington.max1@gmail.com>
2020-09-13 18:04:54 -0300

    Criação de suite de teste.

---
+++ src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
@@ -0,0 +1,17 @@
+package br.ce.wcaquino.suites;
+
+import br.ce.wcaquino.servicos.CalculadoraTest;
+import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CalculadoraTest.class,
+        CalculoValorLocacaoTest.class,
+        LocacaoServiceTest.class
+})
+public class SuiteExecucao {
+    // Criação de classe Suite, serve para centralizar os testes, ordens a serem executadas
+}
