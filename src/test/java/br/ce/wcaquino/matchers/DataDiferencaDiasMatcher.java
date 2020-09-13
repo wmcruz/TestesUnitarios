eba68dc64041a7095e8819ebb405805c4f933b3c
Wellington Max Cruz <wellington.max1@gmail.com>
2020-09-13 14:07:35 -0300

    Novos matchers mais legiveis.

---
+++ src/test/java/br/ce/wcaquino/matchers/DataDiferencaDiasMatcher.java
@@ -0,0 +1,26 @@
+package br.ce.wcaquino.matchers;
+
+import br.ce.wcaquino.utils.DataUtils;
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+import java.util.Date;
+
+public class DataDiferencaDiasMatcher extends TypeSafeMatcher<Date> {
+
+    private Integer qtdDias;
+
+    public DataDiferencaDiasMatcher(Integer qtdDias) {
+        this.qtdDias = qtdDias;
+    }
+
+    @Override
+    protected boolean matchesSafely(Date data) {
+        return DataUtils.isMesmaData(data, DataUtils.obterDataComDiferencaDias(qtdDias));
+    }
+
+    @Override
+    public void describeTo(Description description) {
+
+    }
+}
