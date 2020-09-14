3247a1ef9cf674f1c4225a53118bd7c08794be0a
Wellington Max Cruz <wellington.max1@gmail.com>
2020-09-13 23:52:27 -0300

    Troca de system out por Assert.

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,5 +1,6 @@
 package br.ce.wcaquino.servicos;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -11,6 +12,6 @@ public class CalculadoraMockTest {
         Mockito.when(calculadora.somar(Mockito.anyInt(), Mockito.anyInt())).thenReturn(5); // testa a soma de qualquer valor inteiro com outro qualquer inteiro
         Mockito.when(calculadora.somar(Mockito.eq(1), Mockito.anyInt())).thenReturn(5); // testa a soma de um valor fixo com outro qualquer inteiro
 
-        System.out.println(calculadora.somar(1, 1000));
+        Assert.assertEquals(5, calculadora.somar(1, 1000));
     }
 }
