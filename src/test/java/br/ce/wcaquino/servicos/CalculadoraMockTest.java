4d90eae8a69f9e1abc7b01ce9774de84a36dd21b
Wellington Max Cruz <wellington.max1@gmail.com>
2020-09-16 22:04:55 -0300

    Implementando um test com spy, e comparando com o mock.

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,10 +1,35 @@
 package br.ce.wcaquino.servicos;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class CalculadoraMockTest {
+
+    @Mock
+    private Calculadora calculadoraMock; // mock realiza o teste da implementação(interface)
+
+    @Spy
+    private Calculadora calculadoraSpy; // spy realiza o teste da execução do método
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void deveMostrarDiferencaEntreMockSpy() {
+        Mockito.when(calculadoraMock.somar(1, 2)).thenReturn(8);
+        // Mockito.when(calculadoraSpy.somar(1, 2)).thenReturn(8); // aqui o java vai executar o metodo somar, podendo ocorrer um nullpointer e etc..
+        Mockito.doReturn(8).when(calculadoraSpy).somar(1, 2); // forma correta de utilizar o spy
+
+        System.out.println("Mock: " + calculadoraMock.somar(1, 5));
+        System.out.println("Spy: " + calculadoraSpy.somar(1, 5));
+    }
 
     @Test
     public void testeDeMockParaCalculadoraSoma() {
